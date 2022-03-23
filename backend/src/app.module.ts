e2f378d4ef41c8728dd92842fe5f92da2d5c4b04
hu9osaez <hg.saezs@gmail.com>
2022-03-23 00:55:21 -0300

    feat(backend): remove unused files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApiModule } from './api.module';
 
 import { checkConfig } from './shared/env.validation';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
   imports: [
     ConfigModule.forRoot({
       cache: true,
