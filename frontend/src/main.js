84a5323115a6b9e03812059de4774cca8767ad43
hu9osaez <hg.saezs@gmail.com>
2022-03-25 14:11:03 -0300

    feat(frontend): get data from API to set on frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,24 @@
 import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import Axios from 'axios';
+import VueAxios from 'vue-axios';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+import 'ant-design-vue/dist/antd.css';
+
+const axios = Axios.create({
+  baseURL: 'http://localhost:3000/',
+});
+
+const app = createApp(App);
+
+app
+  .use(Antd)
+  .use(VueAxios, axios)
+  .use(store)
+  .use(router)
+  .provide('axios', app.config.globalProperties.axios)
+  .mount('#app');
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main-container">
+    <a-row justify="center">
+      <a-col :span="12" class="main-box">
+        <a-space>
+          <a-select
+            ref="slYears"
+            v-model:value="selectedYear"
+            style="width: 200px"
+            :options="yearsList"
+            placeholder="Selecciona un año"
+          ></a-select>
+        </a-space>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import {
+  defineComponent,
+  onMounted,
+  inject,
+  ref,
+  watch,
+} from 'vue';
 
-export default {
+export default defineComponent({
   name: 'HomeView',
-  components: {
-    HelloWorld,
+  setup() {
+    const axios = inject('axios');
+    const statsYear = ref(null);
+    const selectedYear = ref(null);
+    const yearsList = ref([]);
+
+    onMounted(async () => {
+      const { data: rawMetadata } = await axios.get('/invoices/misc/metadata');
+
+      yearsList.value = Array.from(rawMetadata.data).map((year) => ({
+        value: year,
+        label: year,
+      }));
+    });
+
+    watch(() => selectedYear.value, async (newValue) => {
+      console.info('NV', newValue);
+      const { data: rawStats } = await axios.get('/invoices/misc/monthly', { params: { year: newValue } });
+      statsYear.value = rawStats.data;
+    });
+
+    return {
+      statsYear,
+      selectedYear,
+      yearsList,
+    };
   },
-};
+});
 </script>
+
+<style lang="scss">
+.main-container {
+  margin-top: 100px;
+}
+.main-box {
+  background: white;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1);
+  padding: 15px 10px;
+}
+</style>
